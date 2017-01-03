c1fc08cfcc325f7271f800ccce5105a9530de0bd
Andrew Golightly <support@andrewgolightly.com>
2017-01-03 13:12:58 +0700

    added core bootstrap css

--- src/index.jsx
+++ src/index.jsx
@@ -5,4 +5,6 @@ import YourComponent from './components/index';
 // remove margins
 document.body.style.margin = 0;
 
+require('bootstrap/dist/css/bootstrap.css');
+
 ReactDOM.render(<YourComponent />, document.getElementById('app'));
