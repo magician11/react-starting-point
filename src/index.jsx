729e0a09c18dff5e8444e5c97f5815e91c9723e0
Andrew Golightly <support@andrewgolightly.com>
2017-02-17 11:41:39 +0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import YourApp from './components/index';
-import About from './components/about';
-import People from './components/people';
-import Person from './components/person';
-import NoMatch from './components/unknown';
-import Home from './components/home';
+import MyApp from './components';
 
-// remove margins
-document.body.style.margin = 0;
-
-require('bootstrap/dist/css/bootstrap.css');
-
-ReactDOM.render((<Router history={browserHistory}>
-  <Route path="/" component={YourApp}>
-    <IndexRoute component={Home} />
-    <Route path="about" component={About} />
-    <Route path="people" component={People}>
-      <Route path="/person/:personId" component={Person} />
-    </Route>
-    <Route path="*" component={NoMatch} />
-  </Route>
-</Router>
-), document.getElementById('app'));
+ReactDOM.render(
+  <MyApp />,
+  document.querySelector('#app')
+);
